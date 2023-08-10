4335830ea858d4c397da1baf378c7f424d1cc86f
kguyer <kguyer@harding.edu>
2023-08-10 12:44:44 -0500

    Add css, migrate, and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "kittens#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :kittens
 end
